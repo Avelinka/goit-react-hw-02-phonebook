664cc53925d0ad429ad8137f3e575ece6b72907a
Avelinka <131503535+Avelinka@users.noreply.github.com>
2023-11-17 06:33:06 +0200

    add task

--- src/components/Layout.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Main = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: ${p => p.theme.spacing(10)};
-  padding: ${p => p.theme.spacing(20)} ${p => p.theme.spacing(4)};
-`;
